2cb416a8331431db7c5c3bed4c336cef68f5a34a
unknown <jithinbp007@gmail.com>
2024-05-04 12:02:57 +0530

    http method -get

---
+++ 13-http-methods/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const users = require("./data");
+
+const app = express();
+
+// get method -> read data
+app.get("/api/users", (req, res) => {
+  res.send(users);
+});
+
+app.listen(3000, () => console.log("Server starts running at port 3000.."));
